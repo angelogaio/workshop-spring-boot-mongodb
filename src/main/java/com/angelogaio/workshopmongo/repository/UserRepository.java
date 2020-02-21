792f8d202ad4a027bd3fae65089a6fd797daf1a3
Angelo Gaio <angelo.gaio@hotmail.com>
2020-02-20 22:20:11 -0300

    Conecção mongodb

---
+++ src/main/java/com/angelogaio/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.angelogaio.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.angelogaio.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+	
+
+}
---
+++ src/main/java/com/angelogaio/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.angelogaio.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.angelogaio.workshopmongo.domain.User;
+import com.angelogaio.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
